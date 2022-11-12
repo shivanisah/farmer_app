7171eae116d7fcf07b995ed8ebc90d3251042114
unknown <shivanisah145@gmail.com>
2022-11-11 19:57:08 -0800

    login screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:agriculture/register.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Register(),
+    );
+  }
+}
